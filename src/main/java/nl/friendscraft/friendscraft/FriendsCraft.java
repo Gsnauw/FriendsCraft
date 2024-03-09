1059458c52ec9707b8027a301a4a44bfa5bc5930
Gilles <gillessnauwaert@gmail.com>
2024-03-09 17:02:16 +0100

    versie 0.0.1

--- src/main/java/nl/friendscraft/friendscraft/FriendsCraft.java
+++ src/main/java/nl/friendscraft/friendscraft/FriendsCraft.java
@@ -1,12 +1,8 @@
 package nl.friendscraft.friendscraft;
 
-import com.earth2me.essentials.IEssentials;
 import nl.friendscraft.friendscraft.commands.AdminCommand;
-import nl.friendscraft.friendscraft.commands.MaintenanceCommand;
-import nl.friendscraft.friendscraft.configs.MaintenanceConfig;
 import nl.friendscraft.friendscraft.configs.MessageConfig;
 import nl.friendscraft.friendscraft.events.*;
-import nl.friendscraft.friendscraft.utils.ChatUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import nl.friendscraft.friendscraft.configs.DefaultConfig;
@@ -15,9 +11,7 @@ public final class FriendsCraft extends JavaPlugin {
 
     private DefaultConfig defaultConfig;
     private MessageConfig messageConfig;
-    private MaintenanceConfig maintenanceConfig;
     static FriendsCraft friendsCraft;
-    private IEssentials essentials;
 
     @Override
     public void onEnable() {
@@ -25,22 +19,11 @@ public final class FriendsCraft extends JavaPlugin {
 
         defaultConfig = new DefaultConfig("config.yml",this);
         messageConfig = new MessageConfig("message.yml",this);
-        maintenanceConfig = new MaintenanceConfig("maintenance.yml",this);
-
-        if (statusEssentials()) {
-            this.essentials = (IEssentials) Bukkit.getPluginManager().getPlugin("Essentials");
-            ChatUtil.sendConsolePrefixInfo("Connectie met Essentials gemaakt.");
-        }
-        if (!statusEssentials()) {
-            ChatUtil.sendConsolePrefixError("Essentials plugin niet gevonden. Maintenance commands add, remove en list uitschakelen...");
-        }
 
         this.getCommand("friendscraftadmin").setExecutor(new AdminCommand());
-        this.getCommand("maintenance").setExecutor(new MaintenanceCommand());
         this.getServer().getPluginManager().registerEvents(new DoubleShulkerShells(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
-        this.getServer().getPluginManager().registerEvents(new PlayerLogin(), this);
 
         Bukkit.getServer().getLogger().info("[Friends-Craft] Plugin enabled, Hello World");
     }
@@ -61,15 +44,6 @@ public final class FriendsCraft extends JavaPlugin {
     public void reload() {
         defaultConfig.reload();
         messageConfig.reload();
-        maintenanceConfig.reload();
-    }
-
-    public IEssentials getEssentials() {
-        return this.essentials;
-    }
-
-    public void reloadMaintenance() {
-        maintenanceConfig.reload();
     }
 
     public static FriendsCraft getInstance() {
