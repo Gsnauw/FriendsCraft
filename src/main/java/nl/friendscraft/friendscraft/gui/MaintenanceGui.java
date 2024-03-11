f69f760cca352579f43fe482d46cb3b1bf3a1057
Gilles <gillessnauwaert@gmail.com>
2024-03-11 22:14:32 +0100

    Maintenance Menu work in progress

--- src/main/java/nl/friendscraft/friendscraft/gui/MaintenanceGui.java
+++ src/main/java/nl/friendscraft/friendscraft/gui/MaintenanceGui.java
@@ -1,7 +1,5 @@
 package nl.friendscraft.friendscraft.gui;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
 import nl.friendscraft.friendscraft.utils.ChatUtil;
 import nl.friendscraft.friendscraft.utils.Gui;
 import org.bukkit.Bukkit;
@@ -10,43 +8,55 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
-
-import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.UUID;
 
 public class MaintenanceGui {
 
     public static void openGui(Player player) {
-        Inventory maintenanceMainGui = Bukkit.createInventory(player, 27, ChatUtil.format("&6&lMaintenance &e&lMenu"));
+        Inventory maintenanceMainGui = Bukkit.createInventory(player, 36, ChatUtil.format("&6&lMaintenance &e&lMenu"));
 
         ItemStack spelerHead = new ItemStack(Material.PLAYER_HEAD);
         ItemMeta spelerMeta = spelerHead.getItemMeta();
 
-        spelerHead.setLore(Arrays.asList(ChatUtil.format("&7Voeg een speler toe aan de maintenance whitelist.")));
-        spelerHead.setDisplayName(ChatUtil.format("&6Spelers"));
-        spelerHead.setItemMeta(spelerHead);
+        spelerMeta.setLore(Arrays.asList(ChatUtil.format("&7Het speler menu.")));
+        spelerMeta.setLore(Arrays.asList(ChatUtil.format("&6Bekijk of wijzig de spelers op de whitelist.")
+                , ChatUtil.format("&6&l(&e&lKlik!&6&l)")));
+        spelerMeta.setDisplayName(ChatUtil.format("&9-> &6Spelers &9<-"));
+        spelerHead.setItemMeta(spelerMeta);
 
-        maintenanceMainGui.setItem(2, addHead);
+        maintenanceMainGui.setItem(16, spelerHead);
 
-        ItemStack listHead = new ItemStack(Material.ANVIL);
-        SkullMeta listMeta = Gui.maakCustomHead(player, listHead, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTdlZDY2ZjVhNzAyMDlkODIxMTY3ZDE1NmZkYmMwY2EzYmYxMWFkNTRlZDVkODZlNzVjMjY1ZjdlNTAyOWVjMSJ9fX0=");
 
-        listMeta.setLore(Arrays.asList(ChatUtil.format("&7Klik hier om de status te wijzigen")));
-        listMeta.setDisplayName(ChatUtil.format("&6Status"));
-        listHead.setItemMeta(listMeta);
+        ItemStack statusItem = new ItemStack(Material.ANVIL);
+        ItemMeta statusMeta = statusItem.getItemMeta();
 
-        maintenanceMainGui.setItem(4, listHead);
+        statusMeta.setLore(Arrays.asList(ChatUtil.format("&7Klik hier om de status te wijzigen")
+                , ChatUtil.format("&6&l(&e&lKlik!&6&l)")));
+        statusMeta.setDisplayName(ChatUtil.format("&9-> &6Status &9<-"));
+        statusItem.setItemMeta(statusMeta);
 
-        ItemStack removeHead = new ItemStack(Material.PLAYER_HEAD);
-        SkullMeta removeMeta = Gui.maakCustomHead(player, removeHead, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGU0YjhiOGQyMzYyYzg2NGUwNjIzMDE0ODdkOTRkMzI3MmE2YjU3MGFmYmY4MGMyYzViMTQ4Yzk1NDU3OWQ0NiJ9fX0=");
+        maintenanceMainGui.setItem(10, statusItem);
 
-        removeMeta.setLore(Arrays.asList(ChatUtil.format("&7Verwijder een speler van de maintenance whitelist.")));
-        removeMeta.setDisplayName(ChatUtil.format("&cRemove &6Speler"));
-        removeHead.setItemMeta(removeMeta);
+        ItemStack bookItem = new ItemStack(Material.BOOK);
+        ItemMeta bookMeta = bookItem.getItemMeta();
 
-        maintenanceMainGui.setItem(6, removeHead);
+        bookMeta.setLore(Arrays.asList(ChatUtil.format("&eDit is het maintenance menu.")
+                , ChatUtil.format("&eBepaal hier de whitelisted spelers")
+                , ChatUtil.format("&eof verander de maintenance status.")));
+        bookMeta.setDisplayName(ChatUtil.format("&9-> &6Info &9<-"));
+        bookItem.setItemMeta(bookMeta);
+
+        maintenanceMainGui.setItem(13, bookItem);
+
+        ItemStack barrierItem = new ItemStack(Material.BARRIER);
+        ItemMeta barrierMeta = barrierItem.getItemMeta();
+
+        barrierMeta.setLore(Arrays.asList(ChatUtil.format("&6Sluit dit menu.")
+                , ChatUtil.format("&6&l(&e&lKlik!&6&l)")));
+        barrierMeta.setDisplayName(ChatUtil.format("&9-> &cSluit &9<-"));
+        barrierItem.setItemMeta(barrierMeta);
+
+        maintenanceMainGui.setItem(31, barrierItem);
 
 
         Gui.fillGui(maintenanceMainGui);
